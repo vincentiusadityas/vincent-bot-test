f3e1f7abf1a80104cfae0e440402cecd5b4db654
Vincentius Aditya Sundjaja <vasundjaja@gmail.com>
2017-10-04 22:54:34 +0700

    try get user id in group

--- echobot/views.py
+++ echobot/views.py
@@ -17,10 +17,9 @@ handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-
+    userText = event.message.text
     if "siapa" in userText.lower():
         if isinstance(event, SourceGroup):
-            userText = event.message.text
             groupId = event.source.group_id
             member_ids_res = line_bot_api.get_group_member_ids(groupId)
             userId = member_ids_res.member_ids[random.randint(0, member_ids_res.member_ids.length-1)]
