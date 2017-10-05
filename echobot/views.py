c10a1a844abcce3bb7b4881f6db112b0614a7db8
Vincentius Aditya Sundjaja <vasundjaja@gmail.com>
2017-10-05 17:48:36 +0700

    test

--- echobot/views.py
+++ echobot/views.py
@@ -14,7 +14,7 @@ from linebot.models import MessageEvent, TextSendMessage, TextMessage
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 
-yesNo = [iya, ngga]
+yesNo = ["iya", "ngga"]
 groupDict = {}
 
 @handler.add(MessageEvent, message=TextMessage)
