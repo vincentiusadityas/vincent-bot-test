f992028e134a16eec086646b442fbb3d00b8b755
Vincentius Aditya Sundjaja <vasundjaja@gmail.com>
2017-10-05 17:46:50 +0700

    test

--- echobot/views.py
+++ echobot/views.py
@@ -14,26 +14,58 @@ from linebot.models import MessageEvent, TextSendMessage, TextMessage
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 
+yesNo = [iya, ngga]
+groupDict = {}
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    print(event.source)
+
     userText = event.message.text
-    print("siapa" in userText.lower())
+
+    groupId = event.source.group_id
+    userId = event.source.user_id
+
+    if groupId not in groupDict:
+        groupDict[groupId] = [userId]
+    else:
+        groupDict[groupId].append(userId)
+
     if "siapa" in userText.lower():
-        groupId = event.source.sender_id
-        print(groupId)
+        name = groupDict[groupId][random.randint(0, groupDict[groupId].length-1)]
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=groupId)
+            TextSendMessage(text=name)
         )
-        member_ids_res = line_bot_api.get_group_member_ids(group_id=groupId)
-        print(member_ids_res.member_ids)
-        print(member_ids_res.next)
-        userId = member_ids_res.member_ids[random.randint(0, member_ids_res.member_ids.length-1)]
+
+    elif "apakah" in userText.lower():
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=userId)
+            TextSendMessage(text=yesNo[random.randint(0,1)])
+            )
+
+    elif "main yuk" in userText.lower():
+        buttons_template_message = TemplateSendMessage(
+            alt_text='Buttons template',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://example.com/image.jpg',
+                title='Menu',
+                text='Please select',
+                actions=[
+                    PostbackTemplateAction(
+                        label='postback',
+                        text='postback text',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message',
+                        text='message text'
+                    ),
+                    URITemplateAction(
+                        label='uri',
+                        uri='http://example.com/'
+                    )
+                ]
+            )
         )
 
     else:
